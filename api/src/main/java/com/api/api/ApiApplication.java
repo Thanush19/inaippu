5e30f2d892870babf6b5cf8f0ee98cf4a33587ad
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-01-07 08:17:41 +0530

    cors in main fn

--- api/src/main/java/com/api/api/ApiApplication.java
+++ api/src/main/java/com/api/api/ApiApplication.java
@@ -3,6 +3,9 @@ package com.api.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ApiApplication {
@@ -11,4 +14,18 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8080", "http://localhost:5173", "https://inaippu.vercel.app")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("Authorization", "Content-Type", "Accept")
+						.allowCredentials(true)
+						.maxAge(3600);
+			}
+		};
+	}
 }
