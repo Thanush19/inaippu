e0d75169119469d35399e780a2cfe292e6651c0f
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-01-07 10:02:02 +0530

    added cors

--- api/src/main/java/com/api/api/ApiApplication.java
+++ api/src/main/java/com/api/api/ApiApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
@@ -14,13 +16,19 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("*")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-				.allowedHeaders("Authorization", "Content-Type", "Accept")
-				.allowCredentials(true)
-				.maxAge(3600);
-	}
+	@Configuration
+	@EnableWebMvc
+	public class MyConfiguration implements WebMvcConfigurer {
 
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			System.out.println("CORS Configuration invoked.");
+			registry.addMapping("/**")
+					.allowedOrigins("http://localhost:5173", "https://inaippu.vercel.app")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowedHeaders("Authorization", "Content-Type", "Accept")
+					.allowCredentials(true)
+					.maxAge(3600);
+		}
+	}
 }
