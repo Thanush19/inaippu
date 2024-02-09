80eb1a910d41c815a306cf5b81b5c1b0978a3492
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-02-09 11:15:32 +0530

    trial api

---
+++ api/src/main/java/com/api/api/entity/Comment.java
@@ -0,0 +1,46 @@
+package com.api.api.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by_user_id")
+    private User createdByUser;
+
+    @ManyToOne
+    @JoinColumn(name = "commented_on_user_id")
+    private User commentedOnUser;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+
+
+
+}
